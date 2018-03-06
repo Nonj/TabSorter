666f795e3b51e45ac7baf747897b20063ea8dcf6
Nattanon Bunyatipanon <nattanon33@yahoo.com>
2018-03-06 14:41:47 -0800

    finished usage button

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,9 @@
 state = {}
 document.addEventListener('DOMContentLoaded', function () {
     var button = document.getElementById('buttonT');
-    console.log(button);
-    console.log("button sent");
 
     // onClick's logic below:
     button.addEventListener('click', function () {
-        console.log(chrome.extension.getBackgroundPage().tabState);
         chrome.extension.getBackgroundPage().tabState;
         chrome.tabs.query({
             currentWindow: true
@@ -31,10 +28,52 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    var button = document.getElementById('newButton2');
+    var button = document.getElementById('buttonU'),
+        state = chrome.extension.getBackgroundPage().tabState;
 
     // onClick's logic below:
     button.addEventListener('click', function () {
 
+        chrome.tabs.query({
+            currentWindow: true
+        }, function(tabs) {
+            if (tabs.length > 0) { 
+                tabs.sort(function (a, b) {
+                    var keyA = state[a.id],
+                        keyB = state[b.id];
+                    if (keyA < keyB) return 1;
+                    if (keyA > keyB) return -1;
+                    return 0;
+                });
+                for (var i = 0; i < tabs.length; ++i) {
+                    // Current tab is pinned, so decrement the tabIndex by one.
+                    chrome.tabs.move(tabs[i].id, {
+                        index: i
+                    });
+                }
+            }
+        })
     });
 });
+
+function sortTabs(tabs, reverse) {
+    if (tabs.length > 0) { 
+        tabs.sort(function (a, b) {
+            var keyA = state[a.id],
+                keyB = state[b.id];
+            if (keyA < keyB) return reverse;
+            if (keyA > keyB) return -reverse;
+            return 0;
+        });
+    }
+    return tabs
+}
+
+function moveTabs(tabs) {
+    for (var i = 0; i < tabs.length; ++i) {
+        // Current tab is pinned, so decrement the tabIndex by one.
+        chrome.tabs.move(tabs[i].id, {
+            index: i
+        });
+    }
+}
