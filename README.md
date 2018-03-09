f702c887450de7a02ff4eff8b47699882f5c1040
Nattanon Bunyatipanon <nattanon33@yahoo.com>
2018-03-08 16:03:32 -0800

    read me done

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # TabSorter
-A chrome extension to sort tabs alphabetically as well as frequency of use.
+## Name: Omar Khan & Nattanon Bunyatipanon
+
+**Purpose:**
+When we both saw how many tabs we had opened on our Chrome window, we knew that there was a problem. There weren't many good or useful solutions online, so we decided to create out own. Alas, we decided that we should create a tab sorter, to sort by the amount of times you use, as well as alphabetically by its title. We chose these two methods because with usage, thats a no brainer, you want the ones you use more to be in the "front", and less relevant ones to be towards the "back". This ensures that your hotkeys can grab the front ones easier. As for alphabetically, it seemed like a standard that a tab sorter should have.
+
+**To Install:**
+1. You want to either download our repo and unzip it, or clone it down to your local machine. 
+2. Now you want to open your Chrome browser, and open up your extensions manager [link](chrome://extensions/). 
+3. Afterwards, click on "Developer Mode" on the top right of the page and click "Load unpacked extension...". 
+4. Navigate to the extension you just recently unzipped and select its folder.
+5. Enable it if it has not already been enabled & reload your browser
+6. Enjoy!
+
+**How To Use:**
+To use our extension is very simple. Just do what you normally would do already and browse the interwebs. Once you feel that it is a bit too cluttered, you can click on our extension and you'll be greeted with two buttons. You can either "Sort By Usage" or "Sort By Title". Now, depending on how often you click in and out of a tab, you may want to sort by usage. Once clicked, your tabs will be rearranged and voila, it's sorted to your liking.
+
+
--- js/popup.js
+++ js/popup.js
@@ -1,72 +1,50 @@
-state = {}
 document.addEventListener('DOMContentLoaded', function () {
     var button = document.getElementById('buttonT');
+    var titleState = {};
 
     // onClick's logic below:
     button.addEventListener('click', function () {
-        chrome.extension.getBackgroundPage().tabState;
         chrome.tabs.query({
             currentWindow: true
         }, function (tabs) {
             if (tabs.length > 0) {
-                tabs.sort(function (a, b) {
-                    var keyA = a.title,
-                        keyB = b.title;
-                    if (keyA < keyB) return -1;
-                    if (keyA > keyB) return 1;
-                    return 0;
-                });
                 for (var i = 0; i < tabs.length; ++i) {
-                    // Current tab is pinned, so decrement the tabIndex by one.
-                    chrome.tabs.move(tabs[i].id, {
-                        index: i
-                    });
+                    var tab = tabs[i];
+                    titleState[tab.id] = tab.title;
                 }
+                sortByState(tabs, -1, titleState);
+                moveTabs(tabs);
             }
         });
     });
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    var button = document.getElementById('buttonU'),
-        state = chrome.extension.getBackgroundPage().tabState;
+    var button = document.getElementById('buttonU');
+    var state = chrome.extension.getBackgroundPage().tabState;
 
     // onClick's logic below:
     button.addEventListener('click', function () {
-
         chrome.tabs.query({
             currentWindow: true
-        }, function(tabs) {
-            if (tabs.length > 0) { 
-                tabs.sort(function (a, b) {
-                    var keyA = state[a.id],
-                        keyB = state[b.id];
-                    if (keyA < keyB) return 1;
-                    if (keyA > keyB) return -1;
-                    return 0;
-                });
-                for (var i = 0; i < tabs.length; ++i) {
-                    // Current tab is pinned, so decrement the tabIndex by one.
-                    chrome.tabs.move(tabs[i].id, {
-                        index: i
-                    });
-                }
+        }, function (tabs) {
+            if (tabs.length > 0) {
+                sortByState(tabs, 1, state);
+                moveTabs(tabs);
             }
         })
     });
 });
 
-function sortTabs(tabs, reverse) {
-    if (tabs.length > 0) { 
-        tabs.sort(function (a, b) {
-            var keyA = state[a.id],
-                keyB = state[b.id];
-            if (keyA < keyB) return reverse;
-            if (keyA > keyB) return -reverse;
-            return 0;
-        });
-    }
-    return tabs
+function sortByState(tabs, reverse, state) {
+    tabs.sort(function (a, b) {
+        // comparison values
+        var keyA = state[a.id],
+            keyB = state[b.id];
+        if (keyA < keyB) return reverse * 1;
+        if (keyA > keyB) return reverse * -1;
+        return 0;
+    });
 }
 
 function moveTabs(tabs) {
